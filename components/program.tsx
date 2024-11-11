bd4bc8f8627d49b25c65de6453e0311918edb70c
Vitalii-250885 <provitaleks@gmail.com>
2024-11-11 16:56:30 +0200

    Update program.tsx

--- components/program.tsx
+++ components/program.tsx
@@ -6,21 +6,25 @@ function Program() {
 			<span className='animation-timeline animate-left text-xl text-gray-400'>
 				Скільки років навчання?
 			</span>
-			<span className='text-3xl animation-timeline animate-left'>4 роки</span>
+			<span className='text-3xl animation-timeline animate-left font-bold'>
+				4 роки
+			</span>
 			<span className='self-end mt-10 text-xl text-gray-400 animation-timeline animate-right'>
 				Скільки сесій у рік?
 			</span>
-			<span className='self-end text-3xl animation-timeline animate-right'>
+			<span className='self-end text-3xl animation-timeline animate-right font-bold'>
 				4 сесії
 			</span>
 			<span className='text-xl mt-10 text-gray-400 animation-timeline animate-left'>
 				Скільки днів сесія?
 			</span>
-			<span className='text-3xl animation-timeline animate-left'>5 днів</span>
+			<span className='text-3xl animation-timeline animate-left font-bold'>
+				5 днів
+			</span>
 			<span className='self-end text-xl mt-10 text-gray-400 animation-timeline animate-right'>
 				Скільки предметів на сесії?
 			</span>
-			<span className='self-end text-3xl animation-timeline animate-right'>
+			<span className='self-end text-3xl animation-timeline animate-right font-bold'>
 				2 предмети
 			</span>
 		</div>
