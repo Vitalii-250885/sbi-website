055221c9ac49497d134ed66f002982fa93df6d47
Vitalii-250885 <provitaleks@gmail.com>
2024-11-11 14:28:45 +0200

    correct contacts section

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -1,22 +1,46 @@
+'use client'
+
+import { Accordion, AccordionItem } from '@nextui-org/react'
 import { Button } from '@nextui-org/button'
 import { Input, Textarea } from '@nextui-org/input'
 import { Link } from '@nextui-org/link'
 
 export default function ContactForm() {
+	const defaultContent =
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
 	return (
-		<div id='contacts' className='flex w-full p-6 mt-20 flex-col gap-4'>
+		<div id='contacts' className='flex w-full px-6 pt-20 pb-6 flex-col gap-4'>
 			<h2 className='text-3xl font-bold text-center'>Контакти</h2>
-			<span>Номери телефонів:</span>
-			<Link className='ml-5 font-bold' color='foreground'>
-				+38 (068)-368-93-07
-			</Link>
-			<Link className='ml-5 font-bold' color='foreground'>
-				+38 (099)-619-31-61
-			</Link>
-			<span>Електронна пошта:</span>
-			<Link className='ml-5 font-bold' color='foreground'>
-				sbi.in.ua@gmail.com
-			</Link>
+
+			<Accordion variant='shadow'>
+				<AccordionItem key='1' title='Номери телефонів'>
+					<Link className='font-bold' color='foreground'>
+						<img src='/phone.png' alt='facebook' className='w-7 mr-2' />
+						+38 (068)-368-93-07
+					</Link>
+					<Link className='font-bold' color='foreground'>
+						<img src='/phone.png' alt='facebook' className='w-7 mr-2' />
+						+38 (099)-619-31-61
+					</Link>
+				</AccordionItem>
+				<AccordionItem key='2' title='Електронна пошта'>
+					<Link className='font-bold' color='foreground'>
+						<img src='/mail.png' alt='facebook' className='w-7 mr-2' />
+						sbi.in.ua@gmail.com
+					</Link>
+				</AccordionItem>
+				<AccordionItem key='3' title='Соціальні мережі'>
+					<Link key={'1'} className='mr-5' color='foreground'>
+						<img src='/facebook.png' alt='facebook' className='w-7 mr-2' />
+						<span className='font-bold'>sbiukraine</span>
+					</Link>
+					<Link key={'2'} color='foreground'>
+						<img src='/instagram.png' alt='facebook' className='w-7 mr-2' />
+						<span className='font-bold'>sbiukr</span>
+					</Link>
+				</AccordionItem>
+			</Accordion>
 
 			<h3 className='text-2xl text-center font-bold mt-5'>Напишіть нам</h3>
 
@@ -24,7 +48,7 @@ export default function ContactForm() {
 				variant='flat'
 				size='lg'
 				type='text'
-				label='І&acute;мя'
+				label='Ім&acute;я'
 				isRequired
 			/>
 			<Input
