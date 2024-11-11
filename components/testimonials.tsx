8a7a2a4710d6e8dfede42cbe5223085b85a3b362
Vitalii-250885 <provitaleks@gmail.com>
2024-11-11 19:36:25 +0200

    add testimonials slider

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,24 +1,52 @@
-import { Image } from '@nextui-org/image'
+'use client'
+
 import React from 'react'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
+
+import { Image } from '@nextui-org/image'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+import css from './testimonials.module.css'
+
+import testimonials from '../data/testimonials.json'
 
 function Testimonials() {
 	return (
-		<div className='mx-3 p-3 mt-20 bg-slate-400 bg-opacity-10 rounded-xl animation-timeline animate-emergence'>
-			<h2 className='text-3xl font-bold text-center'>Відгуки студентів</h2>
-			<div className='pt-6 relative before:content-["\00BB"] before:text-[360px] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:opacity-5'>
-				<div className='flex justify-between'>
-					<Image isBlurred src='irina.jpeg' alt='Irina' className='m-5 w-28' />
-					<p className='mt-5 font-bold text-right text-2xl'>
-						Ірина <br /> Ожеред
-					</p>
-				</div>
-				<p className='mx-5 animation-timeline animate-emergence'>
-					СБІ – це особлива частина мого життя. Місце де Бог по-особливому
-					працює в моєму серці, де я надбала нових друзів. Місце, де я надихаюсь
-					служити іншим людям і присвячувати себе служінню Богу.
-				</p>
+		<>
+			<div className='w-full mx-3 p-3 mt-20 rounded-xl animation-timeline animate-emergence'>
+				<h2 className='text-3xl font-bold text-center'>Відгуки студентів</h2>
+				<Swiper
+					autoplay={{
+						delay: 8000,
+						disableOnInteraction: true,
+					}}
+					modules={[Autoplay]}
+					className='mySwiper'>
+					{testimonials.map(({ photo, firstName, lastName, article }) => (
+						<SwiperSlide>
+							<div className={css.slide}>
+								<div className={css['photo-name']}>
+									<Image
+										isBlurred
+										src={photo}
+										alt={firstName}
+										className={css.photo}
+									/>
+									<p className={css.name}>
+										{firstName} <br /> {lastName}
+									</p>
+								</div>
+								<p className={css.text}>{article}</p>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
 			</div>
-		</div>
+		</>
 	)
 }
 
