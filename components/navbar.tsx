c8ee7779424370a72eaa7fced44634b45ca35e37
Vitalii-250885 <provitaleks@gmail.com>
2024-11-11 15:04:51 +0200

    correct scrollbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,7 +22,7 @@ export const Navbar = () => {
 		<NextUINavbar maxWidth='xl' className='fixed top-0 left-0'>
 			<NavbarContent className='basis-1/5 sm:basis-full' justify='start'>
 				<NavbarBrand as='li' className='gap-3 max-w-fit'>
-					<NextLink className='flex justify-start items-center gap-1' href='/'>
+					<NextLink className='flex justify-start items-center gap-1' href='#'>
 						<Logo width={12} />
 					</NextLink>
 				</NavbarBrand>
