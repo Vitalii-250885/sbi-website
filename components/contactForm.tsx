5972c42667ae22741d774764ce6196057577a6fd
Vitalii-250885 <provitaleks@gmail.com>
2024-11-13 19:58:22 +0200

    Update contactForm.tsx

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -3,12 +3,65 @@
 import { Button } from '@nextui-org/button'
 import { Input, Textarea } from '@nextui-org/input'
 import { Link } from '@nextui-org/link'
+import { useState } from 'react'
+import { title } from './primitives'
 
 export default function ContactForm() {
+	const [isSubmitting, setIsSubmitting] = useState(false)
+	const [formData, setFormData] = useState({
+		name: '',
+		phoneNumber: '',
+		email: '',
+		message: '',
+	})
+	const [isNameValid, setIsNameValid] = useState(true)
+	const [isPhoneNumberValid, setIsPhoneNumberValid] = useState(true)
+	const [isMailValid, setIsMailValid] = useState(true)
+
+	console.log(formData)
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		const form = e.currentTarget
+
+		const name = e.target[0].value
+		const phoneNumber = e.target[1].value
+		const email = e.target[2].value
+		const message = e.target[3].value
+
+		if (name !== '' && phoneNumber !== '' && email !== '') {
+			setIsSubmitting(true)
+			setFormData({
+				name,
+				phoneNumber,
+				email,
+				message,
+			})
+			form.reset()
+			setTimeout(() => setIsSubmitting(false), 10000)
+		}
+
+		if (name === '') {
+			setIsNameValid(false)
+		}
+		if (phoneNumber === '') {
+			setIsPhoneNumberValid(false)
+		}
+		if (email === '') {
+			setIsMailValid(false)
+		}
+
+		setTimeout(() => {
+			setIsNameValid(true)
+			setIsPhoneNumberValid(true)
+			setIsMailValid(true)
+		}, 2000)
+	}
+
 	return (
 		<div
 			id='contacts'
-			className='flex w-full px-6 sm:px-12 pt-20 pb-36 flex-col gap-4'>
+			className='flex w-full px-6 sm:px-12 pt-20 pb-60 flex-col gap-4'>
 			<h2 className='text-3xl font-bold text-center animation-timeline animate-emergence'>
 				Контакти
 			</h2>
@@ -47,43 +100,59 @@ export default function ContactForm() {
 						</Link>
 					</div>
 				</div>
-				<div className='flex flex-col gap-3 mt-6 md:mt-0 w-full'>
-					<Input
-						variant='flat'
-						size='lg'
-						type='text'
-						label="Ім'я"
-						isRequired
-						className='animation-timeline animate-emergence'
-					/>
-					<Input
-						variant='flat'
-						size='lg'
-						type='phone'
-						label='Номер телефону'
-						isRequired
-						className='animation-timeline animate-emergence'
-					/>
-					<Input
-						variant='flat'
-						size='lg'
-						type='email'
-						label='Електронна пошта'
-						isRequired
-						className='animation-timeline animate-emergence'
-					/>
-					<Textarea
-						variant='flat'
-						label='Повідомлення'
-						className='animation-timeline animate-emergence'
-					/>
-					<Button
-						color='primary'
-						variant='solid'
-						className='animation-timeline animate-emergence'>
-						Надіслати
-					</Button>
-				</div>
+				{isSubmitting ? (
+					<div className='flex flex-col items-center justify-center h-[25.4rem] sm:h-[26rem]'>
+						<p className={title({ color: 'green', size: 'md' })}>Дякуємо!</p>
+						<p className='animate-emergence mt-6 text-center'>
+							Твоє повідомлення було надіслано нам. Найближчим часом ми з тобою
+							зв'яжемось!
+						</p>
+					</div>
+				) : (
+					<form
+						onSubmit={handleSubmit}
+						className='flex flex-col gap-3 mt-6 md:mt-0 w-full'>
+						<Input
+							variant='flat'
+							size='lg'
+							type='text'
+							color={isNameValid ? 'default' : 'danger'}
+							label="Ім'я"
+							isRequired
+							className='animation-timeline animate-emergence'
+						/>
+						<Input
+							variant='flat'
+							size='lg'
+							type='phone'
+							color={isPhoneNumberValid ? 'default' : 'danger'}
+							label='Номер телефону'
+							isRequired
+							className='animation-timeline animate-emergence'
+						/>
+						<Input
+							variant='flat'
+							size='lg'
+							type='email'
+							color={isMailValid ? 'default' : 'danger'}
+							label='Електронна пошта'
+							isRequired
+							className='animation-timeline animate-emergence'
+						/>
+						<Textarea
+							variant='flat'
+							label='Повідомлення'
+							className='animation-timeline animate-emergence'
+						/>
+						<Button
+							type='submit'
+							color='primary'
+							variant='solid'
+							className='animation-timeline animate-emergence'>
+							Надіслати
+						</Button>
+					</form>
+				)}
 			</div>
 		</div>
 	)
