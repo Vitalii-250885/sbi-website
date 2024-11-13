46d3830eec2207b30e634eb98b55c5f60097361d
Vitalii-250885 <provitaleks@gmail.com>
2024-11-13 09:47:06 +0200

    change subjects

--- components/subjects.tsx
+++ components/subjects.tsx
@@ -2,14 +2,10 @@
 
 import React from 'react'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay } from 'swiper/modules'
-
-import 'swiper/css'
-import 'swiper/css/pagination'
+import { Accordion, AccordionItem } from '@nextui-org/accordion'
 
 function Subjects() {
-	const subjects = [
+	const doctrines = [
 		'Життя та служіння Ісуса Христа',
 		'Христологія',
 		'Особа та служіння Духа Святого',
@@ -20,23 +16,9 @@ function Subjects() {
 		'Хамартіологія та антропологія',
 		'Еклесіологія',
 		'Есхатологія',
-		'Огляд Старого Заповіту',
-		'Буття, розділи 1-11',
-		'Царі Юдеї та Ізраїлю',
-		'Поезія мудрості',
-		'Книга пророка Ісаї',
-		'Книга Даниїла',
-		'Малі пророки',
-		'Огляд Нового Заповіту',
-		'Чотири Євангелії',
-		'Послання Івана',
-		'Послання до Галатів',
-		'Послання до Єфесян',
-		'Послання до Римлян',
-		'Перше послання до Коринтян',
-		'Перше і друге послання до Тимофія',
-		'Послання до Євреїв',
-		'Об’явлення',
+	]
+
+	const service = [
 		'Біблійна етика',
 		'Біблійне душпастирство',
 		'Історія християнства',
@@ -54,23 +36,72 @@ function Subjects() {
 		'Християнська сім’я',
 	]
 
+	const oldTestament = [
+		'Огляд Старого Заповіту',
+		'Буття, розділи 1-11',
+		'Царі Юдеї та Ізраїлю',
+		'Поезія мудрості',
+		'Книга пророка Ісаї',
+		'Книга Даниїла',
+		'Малі пророки',
+	]
+
+	const newTestament = [
+		'Огляд Нового Заповіту',
+		'Чотири Євангелії',
+		'Послання Івана',
+		'Послання до Галатів',
+		'Послання до Єфесян',
+		'Послання до Римлян',
+		'Перше послання до Коринтян',
+		'Перше і друге послання до Тимофія',
+		'Послання до Євреїв',
+		'Об’явлення',
+	]
+
 	return (
 		<>
-			<div className='w-full mx-3 p-3 pb-0 mt-20 rounded-xl animation-timeline animate-emergence bg-[url("/book.png")] bg-contain bg-no-repeat bg-bottom'>
-				<h2 className='text-3xl font-bold text-center'>Ти Вивчиш:</h2>
-				<Swiper
-					autoplay={{
-						delay: 1500,
-						disableOnInteraction: false,
-					}}
-					modules={[Autoplay]}
-					className='mySwiper'>
-					{subjects.map((subject, index) => (
-						<SwiperSlide key={index} className='subjectSlide'>
-							<div className='text-2xl sm:text-3xl text-center'>{subject}</div>
-						</SwiperSlide>
-					))}
-				</Swiper>
+			<div className='w-full mx-3 p-3 pb-0 mt-20 animation-timeline animate-emergence'>
+				<h2 className='text-3xl mb-6 font-bold text-center'>Ти Вивчиш:</h2>
+				<Accordion variant='shadow'>
+					<AccordionItem key='1' aria-label='Доктрини' title='Доктрини'>
+						{doctrines.map((subject, index) => (
+							<div key={index} className='text-lg sm:text-xl'>
+								{subject}
+							</div>
+						))}
+					</AccordionItem>
+					<AccordionItem
+						key='2'
+						aria-label='Служіння в церкві та особистий духовний ріст'
+						title='Служіння в церкві та особистий духовний ріст'>
+						{service.map((subject, index) => (
+							<div key={index} className='text-lg sm:text-xl'>
+								{subject}
+							</div>
+						))}
+					</AccordionItem>
+					<AccordionItem
+						key='3'
+						aria-label='Книги Старого Заповіту'
+						title='Книги Старого Заповіту'>
+						{oldTestament.map((subject, index) => (
+							<div key={index} className='text-lg sm:text-xl'>
+								{subject}
+							</div>
+						))}
+					</AccordionItem>
+					<AccordionItem
+						key='4'
+						aria-label='Книги Нового Заповіту'
+						title='Книги Нового Заповіту'>
+						{newTestament.map((subject, index) => (
+							<div key={index} className='text-lg sm:text-xl'>
+								{subject}
+							</div>
+						))}
+					</AccordionItem>
+				</Accordion>
 			</div>
 		</>
 	)
