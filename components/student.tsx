3e8e97093f1e20f5dc327ca6dff09a8e6c8cade9
Vitalii-250885 <provitaleks@gmail.com>
2024-11-26 00:26:27 +0200

    add information about student

--- components/student.tsx
+++ components/student.tsx
@@ -5,7 +5,8 @@ import clsx from 'clsx'
 
 function Student() {
   return (
-    <div className='flex px-3 gap-3 justify-between'>
+    <div className='px-3'>
+    <div className='flex gap-3 justify-between'>
     <Image
       isBlurred
       src="/Vova-small.jpg"
@@ -15,10 +16,16 @@ function Student() {
     <div className='flex flex-col items-end'>
      <h3 className={clsx(title({ size: 'sm', color: 'blue' }), 'text-right')}>Володимир <br /> Цап</h3>
      <p className='text-sm mt-5'>Дата вступу</p>
-     <p className='text-sm text-zinc-500'>12.01.2020</p>
+     <p className='text-sm text-zinc-500'>20.08.2020</p>
      <p className='text-sm mt-1'>Дата випуску</p>
      <p className='text-sm text-zinc-500'>-</p>
      </div>
+    </div>
+    <p>Електронна пошта: <a href="mailto:vova@gmail.com" className='text-zinc-500'>vova@gmail.com</a></p>
+    <p>Номер телефону: <a href="tell:+380967167473" className='text-zinc-500'>096-716-74-73</a></p>
+    <p>Адреса: <span className='text-zinc-500'>-</span></p>
+    <p>Церква: <span className='text-zinc-500'>Черче</span></p>
+    <p>Служіння: <span className='text-zinc-500'>Проповідник</span></p>
     </div>
   )
 }
--- components/subjectTable.tsx
+++ components/subjectTable.tsx
@@ -3,6 +3,152 @@ import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@
 
 
 export default function SubjectTable() {
+  const subjects = [
+    {
+      subject: 'Огляд Нового Заповіту',
+      markEx: '5-',
+      markHW: 'Чудово/Чудово',
+      date: '24.08.2020'
+    },
+    {
+      subject: 'Послання до Євреїв',
+      markEx: '4',
+      markHW: 'Чудово',
+      date: '24.08.2020'
+    },
+    {
+      subject: 'Послання до Єфесян',
+      markEx: '5-',
+      markHW: 'Чудово',
+      date: '25.01.2021'
+    },
+    {
+      subject: 'Царі Юдеї і Ізраілю',
+      markEx: '5',
+      markHW: 'Чудово',
+      date: '25.01.2021'
+    },
+    {
+      subject: 'Поезія мудрості',
+      markEx: '5--',
+      markHW: 'Не здане',
+      date: '10.05.2021'
+    },
+    {
+      subject: 'Перше послання до Коринтян',
+      markEx: '4-',
+      markHW: 'Не здане',
+      date: '10.05.2021'
+    },
+    {
+      subject: 'Біблійна етика',
+      markEx: '5+',
+      markHW: 'Не здане',
+      date: '23.08.2021'
+    },
+    {
+      subject: 'Бібліологія',
+      markEx: '5',
+      markHW: 'Чудово',
+      date: '23.08.2021'
+    },
+    {
+      subject: 'Сотеріологія',
+      markEx: '5',
+      markHW: 'Чудово/Чудово',
+      date: '15.11.2021'
+    },
+    {
+      subject: 'Світові релігії',
+      markEx: '5-',
+      markHW: 'Не здане',
+      date: '15.11.2021'
+    },
+    {
+      subject: 'Особистість і служіння Духа Святого',
+      markEx: '5+',
+      markHW: 'Не здане',
+      date: '24.01.2022'
+    },
+    {
+      subject: 'Книга пророка Ісаї',
+      markEx: '5+',
+      markHW: 'Не здане',
+      date: '24.01.2022'
+    },
+    {
+      subject: 'Особистий євангелізм',
+      markEx: '4-',
+      markHW: 'Не здане',
+      date: '30.01.2023'
+    },
+    {
+      subject: 'Перше та друге послання до Тимофія',
+      markEx: '5',
+      markHW: 'Не здане',
+      date: '30.01.2023'
+    },
+    {
+      subject: 'Буття 1-11 глави',
+      markEx: '4',
+      markHW: 'Не здане',
+      date: '24.04.2023'
+    },
+    {
+      subject: 'Життя Ісуса Христа',
+      markEx: '5-',
+      markHW: 'Зараховано',
+      date: '24.04.2023'
+    },
+    {
+      subject: 'Послання Івана',
+      markEx: '4',
+      markHW: 'Чудово',
+      date: '21.08.2023'
+    },
+    {
+      subject: 'Біблійна філософія музики',
+      markEx: '4',
+      markHW: 'Чудово',
+      date: '21.08.2023'
+    },
+    {
+      subject: 'Теологія',
+      markEx: '5+',
+      markHW: 'Не здане',
+      date: '06.11.2023'
+    },
+    {
+      subject: 'Малі пророки',
+      markEx: '4-',
+      markHW: 'Добре',
+      date: '06.11.2023'
+    },
+    {
+      subject: 'Христологія',
+      markEx: '5--',
+      markHW: 'Не здане',
+      date: '05.02.2024'
+    },
+    {
+      subject: 'Герменевтика',
+      markEx: '5-',
+      markHW: 'Не здане',
+      date: '05.02.2024'
+    },
+    {
+      subject: 'Послання до Римлян',
+      markEx: '4-',
+      markHW: 'Не здане',
+      date: '06.05.2024'
+    },
+    {
+      subject: 'Есхатологія',
+      markEx: '5-',
+      markHW: 'Не здане',
+      date: '06.05.2024'
+    },
+  ]
 
   return (
     <div className="flex flex-col gap-3 pt-10 px-3">
@@ -13,31 +159,20 @@ export default function SubjectTable() {
         aria-label="Пройдені предмети"
       >
         <TableHeader>
-          <TableColumn className="w-6/12">Предмет</TableColumn>
-          <TableColumn className="w-3/12">Оцінка екзамен</TableColumn>
-          <TableColumn className="w-3/12">Оцінка Д/З</TableColumn>
+          <TableColumn className="w-2/12">Дата</TableColumn>
+          <TableColumn className="w-8/12">Предмет</TableColumn>
+          <TableColumn className="w-1/12">Оцінка екзамен</TableColumn>
+          <TableColumn className="w-1/12">Оцінка Д/З</TableColumn>
         </TableHeader>
         <TableBody>
-          <TableRow key="1">
-            <TableCell>Життя та служіння Ісуса Xриста</TableCell>
-            <TableCell>5</TableCell>
-            <TableCell>5</TableCell>
+          {subjects.map(subject => (
+            <TableRow key={subject.subject}>
+            <TableCell>{subject.date}</TableCell>
+            <TableCell>{subject.subject}</TableCell>
+            <TableCell>{subject.markEx}</TableCell>
+            <TableCell>{subject.markHW}</TableCell>
           </TableRow>
-          <TableRow key="2">
-            <TableCell>Біблійна етика</TableCell>
-            <TableCell>5</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
-          <TableRow key="3">
-            <TableCell>Огляд Старого Заповіту</TableCell>
-            <TableCell>5</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
-          <TableRow key="4">
-            <TableCell>Послання до Римлян</TableCell>
-            <TableCell>5</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
