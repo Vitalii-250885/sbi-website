99a4964ad413fdeffd10dae1a7539b36adcb3ee0
Vitalii-250885 <provitaleks@gmail.com>
2025-05-14 12:05:24 +0300

    feat: changed session date

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -4,19 +4,19 @@ import { RangeCalendar } from '@nextui-org/calendar'
 import { CalendarDate } from '@internationalized/date'
 
 function CalendarComponent() {
-	let date = new CalendarDate(2025, 2, 3)
+  let date = new CalendarDate(2025, 8, 25)
 
-	return (
-		<RangeCalendar
-			aria-label='Наступна сесія'
-			value={{
-				start: date,
-				end: date.add({ days: 4 }),
-			}}
-			isReadOnly
-			className='animation-timeline animate-emergence'
-		/>
-	)
+  return (
+    <RangeCalendar
+      aria-label='Наступна сесія'
+      value={{
+        start: date,
+        end: date.add({ days: 4 }),
+      }}
+      isReadOnly
+      className='animation-timeline animate-emergence'
+    />
+  )
 }
 
 export default CalendarComponent
