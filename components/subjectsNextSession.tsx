d8551ca14b3ef2cac3c624c9ea8e59700bffaac3
Vitalii-250885 <provitaleks@gmail.com>
2025-05-15 09:50:47 +0300

    feat: changed subjects by next session

--- components/subjectsNextSession.tsx
+++ components/subjectsNextSession.tsx
@@ -1,36 +1,36 @@
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableColumn,
-	TableHeader,
-	TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
 } from '@nextui-org/table'
 import React from 'react'
 
 function SubjectsNextSession() {
-	return (
-		<div className='mt-12 lg:mt-0 animation-timeline animate-emergence'>
-			<Table
-				aria-label='Предмети наступної сесії'
-				className='w-full animation-timeline animate-emergence'>
-				<TableHeader>
-					<TableColumn className='text-center'>Предмет</TableColumn>
-					<TableColumn className='text-center'>Лектор</TableColumn>
-				</TableHeader>
-				<TableBody>
-					<TableRow key='1'>
-						<TableCell>Доктрина освячення</TableCell>
-						<TableCell>Дерек Томас</TableCell>
-					</TableRow>
-					<TableRow key='2'>
-						<TableCell>Книга Об'явлення</TableCell>
-						<TableCell>Павло Парфенюк</TableCell>
-					</TableRow>
-				</TableBody>
-			</Table>
-		</div>
-	)
+  return (
+    <div className='mt-12 lg:mt-0 animation-timeline animate-emergence'>
+      <Table
+        aria-label='Предмети наступної сесії'
+        className='w-full animation-timeline animate-emergence'>
+        <TableHeader>
+          <TableColumn className='text-center'>Предмет</TableColumn>
+          <TableColumn className='text-center'>Лектор</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key='1'>
+            <TableCell>Огляд Старого Завіту</TableCell>
+            <TableCell>Дерек Томас</TableCell>
+          </TableRow>
+          <TableRow key='2'>
+            <TableCell>Послання до Євреїв</TableCell>
+            <TableCell>Павло Парфенюк</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
 
 export default SubjectsNextSession
